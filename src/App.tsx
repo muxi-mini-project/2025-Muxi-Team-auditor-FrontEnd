5c3403b246039070283bcbb9923663ea77b65a66
bingling-sama <bingling_sama@qq.com>
2025-01-11 16:15:28 +0800

    ✨ feat: add antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from 'antd';
+
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      <Button type="primary">Button</Button>
     </>
   );
 }
