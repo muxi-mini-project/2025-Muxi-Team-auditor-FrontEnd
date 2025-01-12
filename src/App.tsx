84cc5f9aa5120b42e1cd841e85d64b6a742776bf
bingling-sama <bingling_sama@qq.com>
2025-01-12 10:31:51 +0800

    🦄 refactor(ui): remove antd
    remove antd to use radix ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import { Suspense } from 'react';
 import { useRoutes } from 'react-router';
 import routes from '~react-pages';
 
-import { Skeleton } from 'antd';
-
 export default function App() {
   return (
     <>
-      <Suspense fallback={<Skeleton />}>{useRoutes(routes)}</Suspense>
+      <Suspense fallback={<div />}>{useRoutes(routes)}</Suspense>
     </>
   );
 }
