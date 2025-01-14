6f9225a7625e5ded77e99048877f53132f2c0daa
bingling-sama <bingling_sama@qq.com>
2025-01-14 16:20:28 +0800

    Revert "✨ feat(components): impl Button style"
    This reverts commit fbcd1e96206060de4c8dac64c2edc804155655d4.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Page() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-background">
-        Hello World!
-      </h1>
+      <h1 className="text-3xl font-bold underline">Hello World!</h1>
       <Link to="/test">
         <Button>Test</Button>
       </Link>
